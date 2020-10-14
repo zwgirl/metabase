e26d7f2b6516863260c78736677e05915da19f2a
Paul Rosenzweig <paul.a.rosenzweig@gmail.com>
2020-10-14 13:42:31 -0400

    contract dashboard grid before animation

--- frontend/src/metabase/dashboard/components/Sidebar.jsx
+++ frontend/src/metabase/dashboard/components/Sidebar.jsx
@@ -10,50 +10,64 @@ const WIDTH = 384;
 
 function Sidebar({ onClose, onCancel, closeIsDisabled, children }) {
   return (
-    <Motion
-      defaultStyle={{ opacity: 0, width: 0 }}
-      style={
-        // isOpen
-        //   ?
-        { opacity: spring(1), width: spring(WIDTH, SPRING_CONFIG) }
-        // : { opacity: spring(0), width: spring(0, SPRING_CONFIG) }
-      }
+    <div
+      style={{
+        width: WIDTH,
+        height: "100%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "flex-end",
+      }}
     >
-      {motionStyle => (
-        <div
-          style={motionStyle}
-          className="flex flex-column border-left bg-white"
-        >
-          <div className="flex flex-column flex-auto overflow-y-scroll">
-            {children}
-          </div>
-          {(onClose || onCancel) && (
-            <div
-              className="flex align-center border-top"
-              style={{
-                paddingTop: 12,
-                paddingBottom: 12,
-                paddingRight: 32,
-                paddingLeft: 32,
-              }}
-            >
-              {onCancel && (
-                <Button small borderless onClick={onCancel}>{t`Cancel`}</Button>
-              )}
-              {onClose && (
-                <Button
-                  primary
-                  small
-                  className="ml-auto"
-                  onClick={onClose}
-                  disabled={closeIsDisabled}
-                >{t`Done`}</Button>
-              )}
+      <Motion
+        defaultStyle={{ opacity: 0, width: 0 }}
+        style={
+          // isOpen
+          //   ?
+          { opacity: spring(1), width: spring(WIDTH, SPRING_CONFIG) }
+          // : { opacity: spring(0), width: spring(0, SPRING_CONFIG) }
+        }
+      >
+        {motionStyle => (
+          <div
+            style={motionStyle}
+            className="flex flex-column border-left bg-white"
+          >
+            <div className="flex flex-column flex-auto overflow-y-scroll">
+              {children}
             </div>
-          )}
-        </div>
-      )}
-    </Motion>
+            {(onClose || onCancel) && (
+              <div
+                className="flex align-center border-top"
+                style={{
+                  paddingTop: 12,
+                  paddingBottom: 12,
+                  paddingRight: 32,
+                  paddingLeft: 32,
+                }}
+              >
+                {onCancel && (
+                  <Button
+                    small
+                    borderless
+                    onClick={onCancel}
+                  >{t`Cancel`}</Button>
+                )}
+                {onClose && (
+                  <Button
+                    primary
+                    small
+                    className="ml-auto"
+                    onClick={onClose}
+                    disabled={closeIsDisabled}
+                  >{t`Done`}</Button>
+                )}
+              </div>
+            )}
+          </div>
+        )}
+      </Motion>
+    </div>
   );
 }
 
